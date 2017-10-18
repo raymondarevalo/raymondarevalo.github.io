cc15949bea61345b4a8a6fd0f1548991ef393a96
raymondarevalo <arevalo.raymond@gmail.com>
2017-10-17 17:41:41 -0700

    Added smooth slider and underline over anchor

--- js/visible.js
+++ js/visible.js
@@ -6,7 +6,7 @@ var project1 = null;
 var project2 = null;
 var project3 = null;
 var project4 = null;
-
+var sections = null;
 
 
 $(document).ready(function(){
@@ -26,12 +26,37 @@ $(document).ready(function(){
   project3 = $('.p3').offset().top;
   project4 = $('.p4').offset().top;
 
+  sections = $('.idSection');
 
   $('.about-section').hide();
   $('.p1').hide();
   $('.p2').hide();
   $('.p3').hide();
   $('.p4').hide();
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top-200
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        //window.location.hash = hash;
+      });
+    } // End if
+  });
+
 });
 
 $(window).scroll(function() {
@@ -101,38 +126,46 @@ $(window).scroll(function() {
 
  });
 
- $('a[href*="#"]')
-   // Remove links that don't actually link to anything
-   .not('[href="#"]')
-   .not('[href="#0"]')
-   .click(function(event) {
-     // On-page links
-     if (
-       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-       &&
-       location.hostname == this.hostname
-     ) {
-       // Figure out element to scroll to
-       var target = $(this.hash);
-       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-       // Does a scroll target exist?
-       if (target.length) {
-         // Only prevent default if animation is actually gonna happen
-         event.preventDefault();
-         $('html, body').animate({
-           scrollTop: target.offset().top
-         }, 1000, function() {
-           // Callback after animation
-           // Must change focus!
-           var $target = $(target);
-           $target.focus();
-           if ($target.is(":focus")) { // Checking if the target was focused
-             return false;
-           } else {
-             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-             $target.focus(); // Set focus again
-           };
-         });
-       }
+ /* start */
+ // $sections incleudes all of the container divs that relate to menu items.
+
+ // The user scrolls
+ $(window).scroll(function(){
+   console.log("Printing here");
+   // currentScroll is the number of pixels the window has been scrolled
+   var currentScroll = $(this).scrollTop();
+   // $currentSection is somewhere to place the section we must be looking at
+   var currentSection = null;
+   console.log("before: " + currentScroll);
+
+   // We check the position of each of the divs compared to the windows scroll positon
+   sections.each(function(){
+     // divPosition is the position down the page in px of the current section we are testing
+     var divPosition = $(this).offset().top;
+
+     // If the divPosition is less the the currentScroll position the div we are testing has moved above the window edge.
+     // the -1 is so that it includes the div 1px before the div leave the top of the window.
+     console.log("divPosition: " + divPosition);
+     console.log("currentScroll: "+ currentScroll);
+
+     if( divPosition-200 - 1 < currentScroll ){
+       // We have either read the section or are currently reading the section so we'll call it our current section
+       currentSection = $(this);
+       console.log("In current Section");
+
+       // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
      }
-   });
+
+     // This is the bit of code that uses the currentSection as its source of ID
+     var id = currentSection.attr('id');
+     console.log(id);
+
+     //console.log(currentSection);
+    $('a').removeClass('active');
+    //$("[href=#"+id+"]").addClass('active');
+    $('[href="#' + id + '"]').addClass('active');
+
+   })
+
+ });
+ /* end */
