8039af039512a16f6dcf4ea42bd204a5e2b419ee
raymondarevalo <arevalo.raymond@gmail.com>
2017-10-19 13:05:36 -0700

    Fixed nav bar, and responsive

--- js/visible.js
+++ js/visible.js
@@ -11,9 +11,9 @@ var sections = null;
 
 $(document).ready(function(){
   var scrollTop     = $(window).scrollTop(),
-      elementOffset = $('.about-section').offset().top,
-      distance      = (elementOffset - scrollTop-400),
-      end           = distance + $('.about-section').height();
+  elementOffset = $('.about-section').offset().top,
+  distance      = (elementOffset - scrollTop-400),
+  end           = distance + $('.about-section').height();
 
 
 
@@ -25,6 +25,11 @@ $(document).ready(function(){
   project2 = $('.p2').offset().top;
   project3 = $('.p3').offset().top;
   project4 = $('.p4').offset().top;
+  $('.about-section').stop(true,false).animate({ opacity: 0 }, 200);
+  $('.p1').stop(true,false).animate({ opacity: 0 }, 200);
+  $('.p2').stop(true,false).animate({ opacity: 0 }, 200);
+  $('.p3').stop(true,false).animate({ opacity: 0 }, 200);
+  $('.p4').stop(true,false).animate({ opacity: 0 }, 200);
 
   sections = $('.idSection');
 
@@ -65,6 +70,14 @@ $(window).scroll(function() {
   var one = screen.height/11;
   var half = screen.height/1.5;
 
+  /* FadeInOut for nav bar */
+  if($(window).scrollTop() > (screen.height-one-one-one)) {
+    $(".header-section").addClass("header-active");
+  } else {
+    //remove the background property so it comes transparent again (defined in your css)
+    $(".header-section").removeClass("header-active");
+  }
+
   /* FadeInOut for HOME SECTION */
   if ($(this).scrollTop() < screenHeight) {
     console.log("Fade In");
@@ -80,92 +93,92 @@ $(window).scroll(function() {
 
   if ($(this).scrollTop() > (aboutTop-half) && $(this).scrollTop() < (aboutTop-one)) {
     $('.about-section').clearQueue();
-    $('.about-section').stop(true,false).animate({ opacity: 1 }, 200);
-  } else {
+    $('.about-section').stop(true,false).animate({ opacity: 1 }, 400);
+  } /*else {
     $('.about-section').clearQueue();
     $('.about-section').stop(true,false).animate({ opacity: 0 }, 200);
-  }
+  }*/
 
   /* FadeInOut for p1 */
 
   if ($(this).scrollTop() > (project1-half) && $(this).scrollTop() < (project1+one)) {
     $('.p1').clearQueue();
-    $('.p1').stop(true,false).animate({ opacity: 1 }, 200);
-  } else {
+    $('.p1').stop(true,false).animate({ opacity: 1 }, 400);
+  } /*else {
     $('.p1').clearQueue();
     $('.p1').stop(true,false).animate({ opacity: 0 }, 200);
-  }
+  }*/
 
   /* FadeInOut for p2 */
   if ($(this).scrollTop() > (project2-half) && $(this).scrollTop() < (project2+one)) {
     $('.p2').clearQueue();
-    $('.p2').stop(true,false).animate({ opacity: 1 }, 200);
-  } else {
+    $('.p2').stop(true,false).animate({ opacity: 1 }, 400);
+  } /*else {
     $('.p2').clearQueue();
     $('.p2').stop(true,false).animate({ opacity: 0 }, 200);
-  }
+  }*/
 
   /* FadeInOut for p3 */
   if ($(this).scrollTop() > (project3-half) && $(this).scrollTop() < (project3-one)) {
     $('.p3').clearQueue();
-    $('.p3').stop(true,false).animate({ opacity: 1 }, 200);
-  } else {
+    $('.p3').stop(true,false).animate({ opacity: 1 }, 400);
+  } /*else {
     $('.p3').clearQueue();
     $('.p3').stop(true,false).animate({ opacity: 0 }, 200);
-  }
+  }*/
 
   /* FadeInOut for p4 */
   if ($(this).scrollTop() > (project4-half) && $(this).scrollTop() < (project4-one)) {
     $('.p4').clearQueue();
-    $('.p4').stop(true,false).animate({ opacity: 1 }, 200);
-  } else {
+    $('.p4').stop(true,false).animate({ opacity: 1 }, 400);
+  } /*else {
     $('.p4').clearQueue();
     $('.p4').stop(true,false).animate({ opacity: 0 }, 200);
-  }
+  }*/
 
 
- });
+});
 
- /* start */
- // $sections incleudes all of the container divs that relate to menu items.
+/* start */
+// $sections incleudes all of the container divs that relate to menu items.
 
- // The user scrolls
- $(window).scroll(function(){
-   console.log("Printing here");
-   // currentScroll is the number of pixels the window has been scrolled
-   var currentScroll = $(this).scrollTop();
-   // $currentSection is somewhere to place the section we must be looking at
-   var currentSection = null;
-   console.log("before: " + currentScroll);
+// The user scrolls
+$(window).scroll(function(){
+  console.log("Printing here");
+  // currentScroll is the number of pixels the window has been scrolled
+  var currentScroll = $(this).scrollTop();
+  // $currentSection is somewhere to place the section we must be looking at
+  var currentSection = null;
+  console.log("before: " + currentScroll);
 
-   // We check the position of each of the divs compared to the windows scroll positon
-   sections.each(function(){
-     // divPosition is the position down the page in px of the current section we are testing
-     var divPosition = $(this).offset().top;
+  // We check the position of each of the divs compared to the windows scroll positon
+  sections.each(function(){
+    // divPosition is the position down the page in px of the current section we are testing
+    var divPosition = $(this).offset().top;
 
-     // If the divPosition is less the the currentScroll position the div we are testing has moved above the window edge.
-     // the -1 is so that it includes the div 1px before the div leave the top of the window.
-     console.log("divPosition: " + divPosition);
-     console.log("currentScroll: "+ currentScroll);
+    // If the divPosition is less the the currentScroll position the div we are testing has moved above the window edge.
+    // the -1 is so that it includes the div 1px before the div leave the top of the window.
+    console.log("divPosition: " + divPosition);
+    console.log("currentScroll: "+ currentScroll);
 
-     if( divPosition-200 - 1 < currentScroll ){
-       // We have either read the section or are currently reading the section so we'll call it our current section
-       currentSection = $(this);
-       console.log("In current Section");
+    if( divPosition-200 - 1 < currentScroll ){
+      // We have either read the section or are currently reading the section so we'll call it our current section
+      currentSection = $(this);
+      console.log("In current Section");
 
-       // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
-     }
+      // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
+    }
 
-     // This is the bit of code that uses the currentSection as its source of ID
-     var id = currentSection.attr('id');
-     console.log(id);
+    // This is the bit of code that uses the currentSection as its source of ID
+    var id = currentSection.attr('id');
+    console.log(id);
 
-     //console.log(currentSection);
+    //console.log(currentSection);
     $('a').removeClass('active');
     //$("[href=#"+id+"]").addClass('active');
     $('[href="#' + id + '"]').addClass('active');
 
-   })
+  })
 
- });
- /* end */
+});
+/* end */
